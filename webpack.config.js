ad188141ab3e610b7053e130463eb7a1ac1957fd
davidsbond <davidsbond93@gmail.com>
2020-04-16 22:45:45 +0100

    Generate manifest.json from package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const CopyPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
@@ -12,7 +13,26 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin([
-      {from: 'manifest.json', to: 'manifest.json'},
+      {
+        from: 'manifest.json',
+        to: 'manifest.json',
+        transform(content) {
+          // Copy name, description & version from the package.json
+          // file and put it in the chrome extension manifest.
+          const manifest = JSON.parse(content.toString());
+          const pkgPath = path.resolve(__dirname, 'package.json');
+          const pkg = JSON.parse(fs.readFileSync(pkgPath));
+
+          manifest['name'] = pkg['name'];
+          manifest['version'] = pkg['version'];
+          manifest['author'] = pkg['author'];
+          manifest['homepage_url'] = pkg['repository'];
+          manifest['description'] = pkg['description'];
+
+          const data = JSON.stringify(manifest, null, 2);
+          return Buffer.from(data);
+        },
+      },
       {from: 'icons', to: 'icons'},
     ]),
   ],
