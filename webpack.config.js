bb8a8cf71c90c111b4e57ed0133d607c3babb308
davidsbond <davidsbond93@gmail.com>
2020-04-18 01:23:34 +0100

    Automatically generate web accessible resources in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
   mode: process.env.NODE_ENV=='production' ? 'production':'development',
   entry: './src/index.js',
+  performance: {
+    // Only emit size warnings in production mode
+    hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
+    assetFilter(assetFilename) {
+      // Don't emit size warnings for wav files.
+      return !(/\.wav$/.test(assetFilename));
+    },
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/index.js',
@@ -23,12 +31,22 @@ module.exports = {
           // file and put it in the chrome extension manifest.
           const manifest = JSON.parse(content.toString());
           const pkgPath = path.resolve(__dirname, 'package.json');
-          const pkg = JSON.parse(fs.readFileSync(pkgPath));
+          const pkg = JSON.parse(fs.readFileSync(pkgPath).toString());
+
+          // Automatically inject audio files into "web_accessible_resources"
+          // section of manifest
+          const resources = [];
+          const audioPath ='audio';
+          fs.readdirSync(path.resolve(__dirname, audioPath))
+              .forEach((file) => {
+                resources.push(path.join(audioPath, file));
+              });
 
           manifest['version'] = pkg['version'];
           manifest['author'] = pkg['author'];
           manifest['homepage_url'] = pkg['repository'];
           manifest['description'] = pkg['description'];
+          manifest['web_accessible_resources'] = resources;
 
           const data = JSON.stringify(manifest, null, 2);
           return Buffer.from(data);
